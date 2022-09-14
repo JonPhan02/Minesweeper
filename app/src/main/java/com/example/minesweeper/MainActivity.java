16a72d53b71d368c118cf16c1496563706f07920
JonPhan02 <phanjona@usc.edu>
2022-09-14 01:28:41 -0700

    Added GridLayout

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import androidx.gridlayout.widget.GridLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 8; j++) {
+                TextView tv = new TextView(this);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setOnClickListener(this::onClickTV);
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.setMargins(5, 5, 5, 5);
+
+                lp.rowSpec = GridLayout.spec(i, 1f);
+
+                lp.columnSpec = GridLayout.spec(j, 1f);
+
+                grid.addView(tv, lp);
+            }
+        }
     }
+
+    private void onClickTV(View view) {
+    }
+
 }
